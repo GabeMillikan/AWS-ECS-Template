7ae399e839fab99c2d0b12ef381dc78ca7861c5b
GabeMillikan <44247924+GabeMillikan@users.noreply.github.com>
2024-06-11 00:29:59 -0700

    remove internet debugging endpoint

--- backend/endpoints/meta/__init__.py
+++ backend/endpoints/meta/__init__.py
@@ -1,3 +1,3 @@
-from . import debug_internet, health, root, stress, test_db
+from . import health, root, stress, test_db
 
-__all__ = ["health", "root", "stress", "test_db", "debug_internet"]
+__all__ = ["health", "root", "stress", "test_db"]
