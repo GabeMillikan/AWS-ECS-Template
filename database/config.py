94d16d547865d411ee813595aea03a0d704a51a9
GabeMillikan <44247924+GabeMillikan@users.noreply.github.com>
2024-06-10 23:28:09 -0700

    remove leaked production credentials
    they've been cycled now, and "production" is a stretch

--- database/config.py
+++ database/config.py
@@ -2,7 +2,7 @@ import os
 
 CONNECTION_STRING = os.getenv(
     "DATABASE_CONNECTION_STRING",
-    "api:p4ssw0rd@template-guide-database.ctw2u8ywk3i1.us-east-2.rds.amazonaws.com:5432/prod",
+    "dev:insecure-local-only@database:5432/dev",
 )
 CONNECTION_URL = f"postgresql+asyncpg://{CONNECTION_STRING}"
 
